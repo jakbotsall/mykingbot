0eb6cbe4c1816872163e85eb27d11df22cb0783b
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-06-30 01:42:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -395,7 +395,7 @@ client.on("message", message => {
       color: 0x06DF00,
       description: "تم مسح الرسائل بنجاح",
       footer: {
-        text: "Name Bot."
+        text: "King | Bot."
       }
     }}).then(msg => {msg.delete(3000)});
                         }
