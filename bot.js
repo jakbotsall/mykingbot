2b4bd7f0dadc27a65154585077e7852fa4497ad6
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-06-30 21:03:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1035,7 +1035,17 @@ client.on('guildMemberAdd', member => {
 
 
 
-
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('*bcall')){
+ if(!message.author.id === '244888652004458497') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 
 
 
