8e6a6f7aa05d36623912391c6af56acd3fd2213a
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-01 20:07:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -491,29 +491,6 @@ Rooms📚 ${client.channels.size} `)
 
 
 
-
-
-
-
-client.on('message', async function (message)  {
-	    const prefix = '*'
-if(message.content.startsWith(prefix+"server")) {
-const vlevel = ['None', 'Low (Must have verified email)', 'Medium (Must be register for 5 mineuts)', 'High (Need to wait 10 minutes)', 'Very High (Need verified phone on account)']
-const members = await message.guild.members.filter(m=> m.presence.status === 'online').size + message.guild.members.filter(m=> m.presence.status === 'idle').size + message.guild.members.filter(m=> m.presence.status === 'dnd').size  
-message.channel.send(new discord.RichEmbed() 
-.setAuthor(`${message.guild.name} [Server Icon URL]`, message.guild.iconURL)
-.setURL(message.guild.iconURL)
-.addField('🆔 Server ID', message.guild.id, true)
-.addField('👑 Server Owner', message.guild.owner, true)
-.addField('🗺 Region', message.guild.region, true)
-.addField(`👥 Members [${message.guild.memberCount}]`, `${members} online` ,true)
-.addField(`💬 Channels`, `**${message.guild.channels.filter(c => c.type === 'category').size}** Categories | **${message.guild.channels.filter(c=> c.type === 'text').size}** Text | **${message.guild.channels.filter(c=> c.type === 'voice').size}** Voice` ,true)
-.addField(`💠 Verification Level`, vlevel[message.guild.verificationLevel] ,true)
-.addField(`👔 Roles`, message.guild.roles.size ,true)
-.addField(`📆 Created On`, message.guild.createdAt ,true)
-)
-}
-})
   
   
   
