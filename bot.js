f7608906a31a1bc233713294cd92ea8431db5875
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-01 20:56:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -587,8 +587,6 @@ const embed = new Discord.RichEmbed()
 ● ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ● 
 :closed_lock_with_key:『اوامـــر الادمـــنـــيــــة』:closed_lock_with_key:
 
-:closed_lock_with_key: *ban 『لتبنيد شخص ما من السيرفر』
-
 :closed_lock_with_key: *kick 『لتعطي شخص كيك』
 
 :closed_lock_with_key: *clear 『لمسح الشات بالرياكشن』
