2f62f51db329dd12b75b8f1f024ae72521718c67
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-01 20:16:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -760,7 +760,33 @@ msg.delete();
    
    
    
-   
+    client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+
+    if(command === "clear") {
+        const emoji = client.emojis.find("name", "log")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("***
+ضع عدد الرسائل التي تريد مسحها :ok_hand:
+***").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("
+php\nعدد الرسائل التي تم مسحها: " + textxt + "\n
+").then(m => m.delete(3000));
+        }    
+    }
+}
+}); 
    
 
    
