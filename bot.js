4449a7cda2c967be100f9013d30a117afe6e53a5
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-01 19:06:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '*'
+const ytdl = require('ytdl-core');
+const request = require('request');
+const devs = ['431150885549113344','244423000802328576'];
+const child_process = require("child_process");
+const adminprefix = "!";
+const fs = require('fs');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+
+const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
+const prefix = '*';
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
+client.user.setGame(`By: Roses`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
-  console.log('+[-----------------------------------------------------------------]+')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
   console.log(`[Start] ${new Date()}`);
-  console.log('+[-----------------------------------------------------------------]+')
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
   console.log('')
-  console.log('+[------------------------------------]+');
+  console.log('╔[════════════════════════════════════]╗');
   console.log(`Logged in as * [ " ${client.user.username} " ]`);
   console.log('')
   console.log('Informations :')
@@ -19,14 +29,56 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log(`servers! [ " ${client.guilds.size} " ]`);
   console.log(`Users! [ " ${client.users.size} " ]`);
   console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('+[------------------------------------]+')
+  console.log('╚[════════════════════════════════════]╝')
   console.log('')
-  console.log('+[------------]+')
+  console.log('╔[════════════]╗')
   console.log(' Bot Is Online')
-  console.log('+[------------]+')
+  console.log('╚[════════════]╝')
   console.log('')
   console.log('')
 });
+
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+    if (message.content === (adminprefix + "Percie")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else     
+    if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(`**${argresult}** : Done :>`)
+  return message.reply("**You Can't Change Your Name ,Only After Two Hours :>**");
+} else
+    if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+        } else     
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  }
+});
+
+
+
+
+
+
+
+
 
 
 
@@ -248,6 +300,9 @@ client.on('message', function(message) {
 		return str.toLowerCase().indexOf('youtube.com') > -1;
 	}
 });
+
+
+
 
 
 
