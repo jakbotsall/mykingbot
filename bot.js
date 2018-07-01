0aa1c83b6796ecf4ef80ed2017d1093462df6591
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-01 20:54:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -645,16 +645,7 @@ message.author.sendEmbed(embed)
 
 
 
-client.on('message', message => {
-if (message.content.startsWith("*ban")) {
-    var mention = message.mentions.members.first();
-    if(!mention) return message.channel.send("يجب منشن العضو");
 
-    mention.ban("By: " + message.author.tag);
-    
-    message.channel.send("تم أعطاء باند الى : " + mention.tag);
-};
-});
 
 
 
