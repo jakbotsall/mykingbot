79145e9b9fa7eda069bfad51319927026ec20f35
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-01 19:54:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -626,6 +626,8 @@ const embed = new Discord.RichEmbed()
 
 :closed_lock_with_key: *bc  『للبرودكاست』 
 
+:closed_lock_with_key: *bcrole  『برودكاست لرتبة معينة』 
+
 :closed_lock_with_key: *deletall  『لحذف كل الرومات و الرولات من السيرفر』 
 
 :closed_lock_with_key: *color  『لانشاء رتب الوان مع اختيار رقم الرتب اللي تبيه』
@@ -661,7 +663,48 @@ message.author.sendEmbed(embed)
 
    
    
-   
+
+
+
+client.on('message' , message => {
+  var prefix = "*";
+  if(message.author.bot) return;
+  if(message.content.startsWith(prefix + "bcrole")) {
+    let args = message.content.split(" ").slice(1);
+
+    if(!args[0]) {
+      message.channel.send("قم بمنشنة الرتبة | *bcrole @everyone رساله");
+        return;
+    }
+    if(!args[1]) {
+      message.channel.send("قم بمنشنة الرتبة | *bcrole @everyone رساله");
+        return;
+    }
+
+      if(args[0] == "@everyone") {
+        message.channel.send(`لقد تم ارسال هذه الرسالة الى ${message.guild.memberCount} اعضاء`);
+        message.guild.members.forEach(mi => {
+          mi.send(
+          "الرسالة :" + "\n" +
+         "**" + `${args[1]}` + "**"
+          );
+        });
+        return;
+      }
+          var role = message.mentions.roles.first();
+            if(!role) {
+              message.reply("لا توجد رتبة بهذا الاسم");
+                return;
+            }
+        message.guild.members.filter(m => m.roles.get(role.id)).forEach(sa => {
+        sa.send(
+          "الرسالة :" + "\n" +
+        "**" + `${args[1]}` + "**"
+          );
+        });
+      message.channel.send(`**لقد تم ارسال هذه الرسالة الى ${message.guild.members.filter(m => m.roles.get(role.id)).size} عضو**`);
+    }
+});
    
    
    
