3321e079d189460ab18ae4239d58f2cd11dfe390
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-02 13:00:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -714,7 +714,7 @@ client.on('message', message => {
 	
 client.on('message',function(message) {
     let messageArray = message.content.split(' ');
-    let muteRole = message.guild.roles.get('اي دي الرتبة') || message.guild.roles.find('name', 'Muted');
+    let muteRole = message.guild.roles.find('name', 'Muted');
     let muteMember = message.mentions.members.first();
     let muteReason = messageArray[2];
     let muteDuration = messageArray[3];
