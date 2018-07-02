664a30ca9290b5e6d88a4b75635230ae2c357857
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-02 12:33:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -271,22 +271,26 @@ client.on('message', function(msg) {
   
   
 client.on('message', message => {
-     if (message.author.bot) return;
-       if (message.content ===  "*invite") {
-           message.channel.send('**تم ارسال الرابط في الخاص**')
-           
+  if (true) {
+if (message.content === '*invite') {
+      message.author.send('  https://goo.gl/ADmgeW  |  تفضل رابط البوت     ').catch(e => console.log(e.stack));
  
- message.author.sendMessage(`
+    }
+   }
+  });
  
  
-
-**
-: رابط مختصر لاضافة البوت
-
-[ https://goo.gl/ADmgeW ]
-**
-`);
-}
+client.on('message', message => {
+     if (message.content === ".invite") {
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#9B59B6")
+  .addField(" Done | تــــم" , " |  تــــم ارســالك في الخــاص")
+     
+     
+     
+  message.channel.sendEmbed(embed);
+    }
 });
   
   
