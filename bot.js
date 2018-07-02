5bd7a7411c984a3c319049ed1fa5ab5cd0cada30
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-02 11:58:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -146,7 +146,23 @@ message.author.sendEmbed(embed)
    
 
 
+client.on("guildCreate", guild => {
+    let embed = new Discord.RichEmbed () 
+    .setTitle('Bot Logs')
+    .addField(' ***Bot joined to :***[' + `${guild.name}` + ']   **By : **' + `${guild.owner.user.username}` + '')
+    .setFooter(':grinning: كفووو:grinning: ')
+    .setTimestamp()
+    client.channels.get("463281820448849920").send(embed)
+  });
 
+  client.on("guildDelete", guild => {
+  let embed = new Discord.RichEmbed ()
+  .setTitle('Bot Logs')
+  .addField(' ***Bot left from :***[' + `${guild.name}` + ']     **By : **' + `${guild.owner.user.username}` +  ' ')
+  .setFooter(':sob: ليييش:sob: ')
+  .setTimestamp()
+  client.channels.get("463281835024318474").send(embed)
+});
 
 
 			 
