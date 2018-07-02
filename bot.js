ee1ec41f3ae87674e19773f838a91dda393f0d05
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-02 16:12:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -834,48 +834,7 @@ var unmuteembeddm = new Discord.RichEmbed()
 
 
 
-
-
-
-client.on("message", message => {
-  if (message.author.bot) return;
-
-  let command = message.content.split(" ")[0];
-
-  if (command === "*unmute") {
-        if (!message.member.hasPermission('MANAGE_ROLES')) return message.reply("** لا يوجد لديك برمشن 'Manage Roles' **").catch(console.error);
-  let user = message.mentions.users.first();
-  let modlog = client.channels.find('name', 'mod-log');
-  let muteRole = client.guilds.get(message.guild.id).roles.find('name', 'Muted');
-  if (!muteRole) return message.reply("** لا يوجد لديك رتبه الميوت'Muted' **").catch(console.error);
-  if (message.mentions.users.size < 1) return message.reply('** يجب عليك منشنت شخص اولاً**').catch(console.error);
-  const embed = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .addField('الأستعمال:', 'اسكت/احكي')
-    .addField('تم فك الميوت عن:', `${user.username}!${user.discriminator} (${user.id})`)
-    .addField('بواسطة:', `${message.author.username}!${message.author.discriminator}`)
-
-  if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.reply('** لا يوجد لدي برمشن Manage Roles **').catch(console.error);
-
-  if (message.guild.member(user).removeRole(muteRole.id)) {
-return message.reply("**:white_check_mark: .. تم فك الميوت عن الشخص **").catch(console.error);
-} else {
-    message.guild.member(user).removeRole(muteRole).then(() => {
-return message.reply("**:white_check_mark: .. تم فك الميوت عن الشخص **").catch(console.error);
-});
-  }
-
-};
-
-});
-	
-
-	
-	
-	
-	
-	
+		
 
 	
 
@@ -1251,7 +1210,7 @@ client.on('guildMemberAdd', member => {
 client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('*bcall')){
+if (message.content.startsWith('*cb')){
  if(!message.author.id === '244888652004458497') return;
 message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
 client.users.forEach(m =>{
