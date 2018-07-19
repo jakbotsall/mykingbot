f85c33912fb4708ce4c68c989b41d40ea7e5e2fb
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-19 14:18:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,7 +132,26 @@ message.author.sendEmbed(embed)
    
    
    
-
+  client.on('message',async message => {
+    if(message.content.startsWith(prefix + "restart")) {
+        if(message.author.id !== "323885452207587329") return message.reply('You aren\'t the bot owner.');
+        message.channel.send('**Restarting.**').then(msg => {
+            setTimeout(() => {
+               msg.edit('**Restarting..**');
+            },1000);
+            setTimeout(() => {
+               msg.edit('**Restarting...**');
+            },2000);
+        });
+        console.log(`${message.author.tag} [ ${message.author.id} ] has restarted the bot.`);
+        console.log(`Restarting..`);
+        setTimeout(() => {
+            client.destroy();
+            client.login('process.env.BOT_TOKEN');
+        },3000);
+    }
+});
+  
 
 
 
