e5f4c8a84cf35a1e8ff9ecd4002391f24bb27b9d
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-20 12:49:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -160,14 +160,6 @@ client.on('message', function(message) {
            msg.delete(5000);
           message.delete(5000);
         });
-    } else if(message.content.startsWith(prefix + "stream")) {
-		        if(message.author.id !== myID) return;
-            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
-        client.user.setGame(args , 'https://twitch.tv/6xlez1');
-        message.channel.send(':white_check_mark: Done!').then(msg => {
-           msg.delete(5000);
-          message.delete(5000);
-        });
     } else if(message.content.startsWith(prefix + "play")) {
 				        if(message.author.id !== myID) return;
             if(!args) return message.reply('اكتب الحالة اللي تريدها.');
