80e85350146f51194e4d2e0a0a47379184908742
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-21 19:11:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -254,6 +254,55 @@ if(!ReBeL.voiceChannel) {
 });
 
 
+client.on('message', message => {
+    if(message.content == prefix + 'servers') {
+             if(!message.author.id === '244888652004458497') return;
+    var gimg;
+    var gname;
+    var gmemb;
+    var gbots;
+    var groles;
+    var servers = client.guilds;
+    servers.forEach((g)=>{
+    gname = g.name;
+    gimg = g.iconURL;
+    gmemb = g.members.size;
+    let serv = new Discord.RichEmbed()
+    .setAuthor(gname,gimg)
+    .setThumbnail(gimg)
+    .addField('Server Member Count',gmemb = g.members.size)
+    .setColor('RANDOM')
+    message.channel.send(`
+    
+            `);
+          message.channel.sendEmbed(serv);
+    }) 
+    }
+    });
+    
+    client.on('message', message => {
+    if(message.content == prefix + 'servers') {
+             if(!message.author.id === '244888652004458497') return;
+    var gimg;
+    var gname;
+    var gmemb;
+    var gbots;
+    var groles;
+    var servers = client.guilds;
+    servers.forEach((g)=>{
+    gname = g.name;
+    gimg = g.iconURL;
+    gmemb = g.members.size;
+    let serv = new Discord.RichEmbed()
+    message.channel.send(`
+    **-------------------------**
+      Server Name : **${gname}**
+      Server MemberCount : **${gmemb} **
+      **---------------------------**
+            `);
+    }) 
+    }
+    });
 
 
 
