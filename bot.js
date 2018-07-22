18cb6f0677e192f275fc00f8414470922627b648
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-22 20:41:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -258,7 +258,7 @@ client.on('message', function(message) {
            msg.delete(5000);
           message.delete(5000);
         });
-    } else if(message.content.startsWith(prefix + "play")) {
+    } else if(message.content.startsWith(prefix + "p")) {
 				        if(message.author.id !== myID) return;
             if(!args) return message.reply('اكتب الحالة اللي تريدها.');
         client.user.setGame(args);
