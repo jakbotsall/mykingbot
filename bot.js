d7bd3f91a0a82871de865236d03e94b281ed6f2b
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-23 17:48:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,51 +56,7 @@ client.on('ready', () => {
 
 
 
-module.exports.run = async (bot, message, args) => {
 
-  //!tempmute @user 1s/m/h/d
-
-  let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!tomute) return message.reply("لم استطع ايجاد الشخص");
-  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("لا استطيع اعطائه ميوت");
-  let muterole = message.guild.roles.find(`name`, "muted");
-  //start of create role
-  if(!muterole){
-    try{
-      muterole = await message.guild.createRole({
-        name: "muted",
-        color: "#000000",
-        permissions:[]
-      })
-      message.guild.channels.forEach(async (channel, id) => {
-        await channel.overwritePermissions(muterole, {
-          SEND_MESSAGES: false,
-          ADD_REACTIONS: false
-        });
-      });
-    }catch(e){
-      console.log(e.stack);
-    }
-  }
-  //end of create role
-  let mutetime = args[1];
-  if(!mutetime) return message.reply("لم يتم تحديد الوقت");
-
-  await(tomute.addRole(muterole.id));
-  message.reply(`<@${tomute.id}> معاقب بميوت لمدة ${ms(ms(mutetime))}`);
-
-  setTimeout(function(){
-    tomute.removeRole(muterole.id);
-    message.channel.send(`<@${tomute.id}> تم فك الميوت عن`);
-  }, ms(mutetime));
-
-
-//end of module
-}
-
-module.exports.help = {
-  name: "tempmute"
-}
 
 
 client.on('guildMemberRemove', (u) => {
