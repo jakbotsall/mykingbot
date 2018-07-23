b55b81eb299e375aca7fdf3944ae726be1a748a0
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-23 21:03:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -831,7 +831,7 @@ client.on('message', message => {
             .addField('``الاسم:crown:``' , `[ ${client.user.tag} ]` , true)
             .addField('``الايدي:id:``' , `[ ${client.user.id} ]` , true)
 			      .addField('``البرفكس:eight_pointed_black_star:``' , `[ * ]` , true)
-			      .setFooter(client.user.avatarURL);
+			      .setFooter(message.user.avatarURL)
     })
 }
 });
