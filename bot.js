9ef2146aa3a1f0f51894b04d0ba6d0feca54bc48
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-23 22:40:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,6 @@ client.on('ready', () => {
 
 
 var servers = {};
-var prefix = '*';
 client.on("message", async message => {
     var args = message.content.substring(prefix.length).split(" ");
     if (!message.content.startsWith(prefix)) return;
