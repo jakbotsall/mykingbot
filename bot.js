3347733d17b90a10951da4c4c4656b2dd232c842
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-24 00:35:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -754,20 +754,6 @@ if (message.content.startsWith(prefix + 'trans')) {
 
 
 
-  client.on('message', message => {
- if (message.content.startsWith(prefix + 'serverinfo')) {
-  const embed = new Discord.RichEmbed()
-  embed.addField('Members', message.guild.memberCount, true)
-  embed.addField('Name', message.guild.name, true)
-  embed.addField('Region', message.guild.region, true)
-  embed.addField('Owner', message.guild.owner, true)
-  embed.addField('ID', message.guild.id, true)
-  embed.setColor(`ff0000`)
-  embed.setThumbnail(message.guild.iconURL)
-  message.channel.sendEmbed(embed)
-}
-
-
 
    client.on('message', message => {
      if (message.content === "*support") {
