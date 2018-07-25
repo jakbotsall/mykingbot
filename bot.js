48817d8b203a0d43aa1aa9dbfd421800e3625315
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-25 21:02:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -338,42 +338,18 @@ setInterval(function(){})
 
 client.on('message', message => {
    if (message.content === "*id") {
-   if(!message.channel.guild) return message.reply(`هذا الأمر فقط ل السيرفرات :x:`);
-	   let member = client.guilds.get(message.guild.id).members.get(message.author.id);
-	   let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-	         var moment = require('moment');
-      var args = message.content.split(" ").slice(1);
-let user = message.mentions.users.first();
-var men = message.mentions.users.first();
- var heg;
- if(men) {
-     heg = men
- } else {
-     heg = message.author
- }
-var mentionned = message.mentions.members.first();
-  var h;
- if(mentionned) {
-     h = mentionned
- } else {
-     h = message.member
- }
-moment.locale('ar-TN');
-      var id = new  Discord.RichEmbed()
-    .setColor("#0a0909")
-    .setAuthor(message.author.username, message.author.avatarURL)
+   let embed = new Discord.RichEmbed()
   .setColor("RANDOM")
+  .setThumbnail(message.author.avatarURL)
   .addField("`: الاسم`",`${message.author.username}`, true)
   .addField('`: التاق`',"#" +  message.author.discriminator, true)
   .addField("`: الايدي`", message.author.id, true)
-  .addField(': عدد الدعوات', inviteCount,false)
   .addField("`: اخر رسالة`", message.author.lastMessage)
   .addField('`: الحالة`', message.author.presence.status)
   .addField("`: تاريخ انشاء الحساب`", message.author.createdAt, true)
-  .setFooter("-")
-   message.channel.sendEmbed(id);
 
-     }
+  message.channel.sendEmbed(embed);
+    }
 });
 
 
