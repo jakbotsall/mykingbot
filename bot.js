5d477ad7657e9f310f03df5185f9154eab7cfdd6
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-07-26 20:38:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -333,7 +333,7 @@ client.channels.get("470259562121920512").sendEmbed(embed)
 
 client.on('ready', function(){
     var ms = 10000 ;
-    var setGame = ['In 73 Server','*help | *invite','In 73 Server','*help | *invite','In 73 Server'];
+    var setGame = ['In 76 Server','*help | *invite','In 76 Server','*help | *invite','In 76 Server'];
     var i = -1;
     var j = 0;
     setInterval(function (){
